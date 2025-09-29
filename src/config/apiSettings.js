4c5bf461c2c7214257814ebed49d3098a42b65c1
MF-patino <m.fpatino@udc.es>
2025-09-30 00:37:55 +0200

    Added GitHub workflow for CD and configuration for automatic deployment

--- src/config/apiSettings.js
+++ src/config/apiSettings.js
@@ -2,4 +2,4 @@
 
 // For production: 'https://herculaneum-transcriptor-latest.onrender.com'
 // For development: 'http://localhost:8080'
-export const API_URL = 'http://localhost:8080';
+export const API_URL = process.env.REACT_APP_API_URL;
